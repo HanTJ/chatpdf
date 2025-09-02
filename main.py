53c5a6165ec6a6209a9459900ffc6e327b1e7cf0
HanTJ <gauguin135@gmail.com>
2025-09-02 17:51:48 +0900

    스트리밍 전달로 변경

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import streamlit as st
 import os
 import tempfile
 from streamlit_extras.buy_me_a_coffee import button
+from langchain.callbacks.base import BaseCallbackHandler
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -26,7 +27,7 @@ openai_key = st.text_input('OPEN_AI_API_KEY', type="password")
 uploaded_file = st.file_uploader("PDF 파일을 올려주세요!", type=['pdf'])
 st.write("---")
 
-button(username="gauguin135", floating=True, width=200)
+button(username="gauguin135", floating=True, width=221)
 
 def pdf_to_document(uploaded_file):
     temp_dir = tempfile.TemporaryDirectory()
@@ -58,17 +59,29 @@ if uploaded_file is not None:
     chromadb.api.client.SharedSystemClient.clear_system_cache()
 
     db = Chroma.from_documents(texts, embeddings_model)
+
+    class StreamHandler(BaseCallbackHandler):
+        def __init__(self, container, initial_text=""):
+            self.container = container
+            self.text=initial_text
+        def on_llm_new_token(self, token: str, **kwargs) -> None:
+            self.text += token
+            self.container.markdown(self.text)
+
     st.header("PDF에게 질문해보세요!!")
     question = st.text_input("질문을 입력하세요")
 
     if st.button("질문하기"):
         with st.spinner('Wait for it...'):
-            llm = ChatOpenAI(temperature=0)
             retriever_from_llm = MultiQueryRetriever.from_llm(
                 retriever=db.as_retriever(), llm=llm
             )
 
             prompt = hub.pull("rlm/rag-prompt")
+
+            chat_box = st.empty()
+            stream_handler = StreamHandler(chat_box)
+            generate_llm = ChatOpenAI(model="gpt-4o-mini", temperature=0, openai_api_key=openai_key, streaming=True, callbacks=[stream_handler])
 
             def format_docs(docs):
                 return "\n\n".join(doc.page_content for doc in docs)
@@ -76,7 +89,7 @@ if uploaded_file is not None:
             rag_chain = (
                 {"context": retriever_from_llm | format_docs, "question": RunnablePassthrough()}
                 | prompt
-                | llm
+                | generate_llm
                 | StrOutputParser()
             )
 
